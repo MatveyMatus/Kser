50fe21e8ca88c69f2587fd1e1e4cce98a66bb469
MatveyMatus-KH101 <91337034+MatveyMatus@users.noreply.github.com>
2021-12-18 02:35:25 +0500

    Add files via upload

---
+++ БинПоиск.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int size_array,
+        key,
+        mid_idx = 0,
+        left = 0,
+        right = 0;
+    left = 10;
+    right = pow(2,31)-1;
+    //cout << right + 10;
+    cin >> key;
+    while (left < right) {
+        //mid_idx = left + (right - left) / 2;
+        mid_idx = (right + left) / 2;
+        cout << mid_idx;
+        if (key <= mid_idx) {
+            right = mid_idx;
+        }
+        else {
+            left = mid_idx + 1;
+        }
+    }
+    cout << right;
+}
+
+
